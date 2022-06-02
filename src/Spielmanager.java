7580237c1ca5ee02ffff042817fcbb411cb09f4a
123654-dev <72887184+123654-dev@users.noreply.github.com>
2022-06-02 20:25:20 +0200

    Spielmanager implementations- und kommentarsession #1

--- src/Spielmanager.java
+++ src/Spielmanager.java
@@ -1,8 +1,34 @@
  
 class Spielmanager {
         
+    //region Attribute
+
+    /** Liste aller teilnehmenden Spieler */
+    static Spieler[] spieler;
+
+    /** Referenz zu der GUI-Instanz, die läuft (Frontend?) */
+    static GUI gui;
+
+    /**Index des aktuellen Spielers in spieler (0-basiert) */
+    static int aktuellerSpieler;
+
+    //endregion
+
+
     public static void main(String[] args) {
+        //Spieleinstellungen initialisieren (Standardwerte siehe Spieleinstellungen#standardWerte())
         Spieleinstellungen einstellungen = Spieleinstellungen.standardWerte();
+
+        //Spieler-Array initialisieren
+        spieler = new Spieler[einstellungen.getMaxSpieleranzahl()];
+
+        //Alle Spieler, die "benötigt" werden, als Objekte erzeugen und das Array damit füllen
+        for (int i = 0; i < spieler.length; i++) {
+            spieler[i] = new Spieler(einstellungen.getStartguthaben());
+        }
+
+        //Neue GUI-Instanz erzeugen, die später verwendet wird
+        gui = new GUI();
         
     }
 }
