a676abfb886b2a03f821f4960436502510a805d2
123654-dev <72887184+123654-dev@users.noreply.github.com>
2022-06-02 20:24:54 +0200

    Spieler aufgeräumt

--- src/Spieler.java
+++ src/Spieler.java
@@ -9,27 +9,19 @@
  */
 public class Spieler
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
 
     /**
-     * Konstruktor für Objekte der Klasse Spieler
+     * Konstruktor fÃ¼r Objekte der Klasse Spieler
      */
-    public Spieler()
+    public Spieler(int pStartguthaben)
     {
-        // Instanzvariable initialisieren
-        x = 0;
+
     }
 
     /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
+     * Wird vom Spielmanager aufgerufen, wenn der Spieler an der Reihe ist.
      */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    public void spielzugAusfuehren() {
+
     }
 }
