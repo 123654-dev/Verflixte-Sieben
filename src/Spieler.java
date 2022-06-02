b41d2a41cf0adca5a17d1d95577b13ed0acc0f3e
paul <paul.pahmeyer@web.de>
2022-06-02 22:21:31 +0200

    Attribute der Klasse Spieler

--- src/Spieler.java
+++ src/Spieler.java
@@ -1,27 +1,45 @@
- 
-
 
 /**
- * Beschreiben Sie hier die Klasse Spieler.
+ * Der Spieler des Spiels "Verflixte Sieben"
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author Paul P
+ * @version 03.06.2022
  */
 public class Spieler
 {
 
+    // Attribute (=Eigenschaften), die der Spieler besitzt
+    private String name;
+    private int punkte;
+    private int vermoegen;
+    private int wurfAnzahl;
+    
     /**
-     * Konstruktor für Objekte der Klasse Spieler
+     * Konstruktor fuer Objekte der Klasse Spieler
      */
-    public Spieler(int pStartguthaben)
+    public Spieler(int pStartguthaben) //String pWuerfel, String pWuerfel2, String pTopf)
     {
-
+        
+        
     }
 
     /**
      * Wird vom Spielmanager aufgerufen, wenn der Spieler an der Reihe ist.
      */
-    public void spielzugAusfuehren() {
+    public void spielzugAusfuehren() 
+    {
+        
+    }
+    /**
+     * Kann aufgerufen werden, um den Punktestand des jeweiligen Spielers anzusehen.
+     */
+    //public int punkteStandAngeben()
 
+    /**
+     * Wird aufgerufen, wenn ein Spieler verloren hat und der Topf geleert werden muss.
+     */
+    public void topfLeeren()
+    {
+        
     }
 }
